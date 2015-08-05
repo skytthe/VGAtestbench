ec8a6047dc4ce4983ee8b66751dde2499ba51ac4
skytthe <skytthe@gmail.com>
2015-08-05 12:13:07 +0200

    vhdl: write-to-file test

--- VGAtestbench/tb_vga.vhd
+++ VGAtestbench/tb_vga.vhd
@@ -2,6 +2,8 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
+use std.textio.all;
+
 --library UNISIM;
 --use UNISIM.VComponents.all;
 
@@ -13,7 +15,9 @@ architecture Behavioral of tb_vga is
 	signal clk_40M          : std_logic := '0';
 
 	signal mem_adr 		: std_logic_vector(18 downto 0) 	:= (others => '0');
-	signal mem_data		: std_logic 							:= '0';
+	signal mem_data		: std_logic_vector(0 downto 0)	:= "0";
+
+	file f : text open write_mode is "data.txt"; 
 
 begin
 
@@ -31,16 +35,22 @@ begin
 		port map(
 			clk_i  => clk_40M,
 			adr_i  => mem_adr,
-			data_o => mem_data
+			data_o => mem_data(0)
 		);
 
 
 	process(clk_40M)
-		variable c : natural := 0;
+		variable c : integer := -1;
+		variable l : line;
+		
 	begin
 		if rising_edge(clk_40M) then
 			c := c +1;
 			mem_adr <= std_logic_vector(to_unsigned(c, mem_adr'length));
+			if c < 800*600 then
+				write(l,to_integer(unsigned(mem_data)));
+				writeline(f,l);
+			end if;
 		end if;
 	end process;
 	
